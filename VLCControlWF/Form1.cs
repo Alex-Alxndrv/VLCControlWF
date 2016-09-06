7c2e05e99f806c70661c87eaf9c49dbf136606d7
Alex <a@alxndrv.ru>
2016-09-06 21:30:45 +0500

    + Eugene Sedov code

--- VLCControlWF/Form1.cs
+++ VLCControlWF/Form1.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Windows.Forms;
 using VLCControlWF.Properties;
-
+//using System;
+using System.IO.Ports;
 namespace VLCControlWF
 {
     public partial class Form1 : Form
     {
+        string str;
         public Form1()
         {
             InitializeComponent();
+           
         }
 
         void SendCommand(string cmd)
@@ -37,6 +40,70 @@ namespace VLCControlWF
         private void buttonNext_Click(object sender, EventArgs e)
         {
             SendCommand(Commands.Next);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true) { 
+            str = serialPort1.ReadLine();
+            serialPort1.ReadExisting();
+            Application.DoEvents();
+                if (label1.Visible == true)
+                {
+                    label1.Visible = false;
+                }
+                else
+                {
+                    label1.Visible = true;
+                }
+            if (str == "1")
+            {
+                SendCommand(Commands.Play);
+                    label1.Text = "PLAY";
+            }
+            if (str == "0")
+            {
+                SendCommand(Commands.Stop);
+                    label1.Text = "STOP";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                open_button.Text = "Открыть";
+                listBox1.Enabled =true;
+                timer1.Stop();
+                serialPort1.Close();
+
+            }
+            else
+            {
+                open_button.Text = "Закрыть";
+                listBox1.Enabled = false;
+                serialPort1.NewLine = "\r";
+            serialPort1.PortName = listBox1.SelectedItem.ToString();
+                
+            serialPort1.Open();
+            timer1.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+        
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+                listBox1.Items.Add(port);
+            }
+            listBox1.SelectedIndex = 0;
+           
         }
     }
 }
